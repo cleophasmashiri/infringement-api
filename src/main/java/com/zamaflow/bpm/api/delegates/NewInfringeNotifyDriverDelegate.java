30a17897891d9741b89c7754d5acff6900d31695
Cleophas Mashiri <cleophas.mashiri@standardbank.co.za>
2020-10-08 16:24:37 +0200

    Update sms

--- src/main/java/com/zamaflow/bpm/api/delegates/NewInfringeNotifyDriverDelegate.java
+++ src/main/java/com/zamaflow/bpm/api/delegates/NewInfringeNotifyDriverDelegate.java
@@ -20,52 +20,55 @@ import org.springframework.stereotype.Component;
 @Component
 public class NewInfringeNotifyDriverDelegate implements JavaDelegate {
 
-    private final Logger LOGGER = LoggerFactory.getLogger(NewInfringeNotifyDriverDelegate.class);
+        private final Logger LOGGER = LoggerFactory.getLogger(NewInfringeNotifyDriverDelegate.class);
 
-    @Autowired
-    private InfringementService infringementService;
+        @Autowired
+        private InfringementService infringementService;
 
-    @Autowired
-    private EmailDispatcher emailDispatcher;
+        @Autowired
+        private EmailDispatcher emailDispatcher;
 
-    @Autowired
-    private SmsSenderImpl smsSenderImpl;
+        @Autowired
+        private SmsSenderImpl smsSenderImpl;
 
-    @Value("${bpm.tasks.baseurl}")
-    public String taskUrl;
+        @Value("${bpm.tasks.baseurl}")
+        public String taskUrl;
 
-    @Value("${bpm.tasks.fromemail}")
-    public String fromEmail;
+        @Value("${bpm.tasks.fromemail}")
+        public String fromEmail;
 
-    @Value("${sms.baseSmtpToSmsUrl}")
-    private String baseSmtpToSmsUrl;
+        @Value("${sms.baseSmtpToSmsUrl}")
+        private String baseSmtpToSmsUrl;
 
-    @Value("${sms.smtpToSmsPassword}")
-    private String smtpToSmsPassword;
+        @Value("${sms.smtpToSmsPassword}")
+        private String smtpToSmsPassword;
 
-    @Value("${sms.enabled}")
-    private String smsEnabled;
+        @Value("${sms.enabled}")
+        private String smsEnabled;
 
-    @Override
-    public void execute(DelegateExecution delegateExecution) throws Exception {
-        LOGGER.info("Sending Infringement Notification for Driver");
-        String driverIdNumber = delegateExecution.getVariable("driverIdNumber").toString();
-        Driver driver = infringementService.findDriverByNationalIdNumber(driverIdNumber);
-        Infringement infringement = infringementService
-                .getInfringmentByprocessInstanceId(delegateExecution.getProcessInstanceId());
-        infringementService.creatInfringementAction(infringement, delegateExecution.getProcessInstanceId(),
-                delegateExecution.getVariable("infringementNotes").toString(),
-                InfringementActionType.INFRINGEMENT_NOTIFICATION_SENT);
+        @Override
+        public void execute(DelegateExecution delegateExecution) throws Exception {
+                LOGGER.info("Sending Infringement Notification for Driver");
+                String driverIdNumber = delegateExecution.getVariable("driverIdNumber").toString();
+                Driver driver = infringementService.findDriverByNationalIdNumber(driverIdNumber);
+                Infringement infringement = infringementService
+                                .getInfringmentByprocessInstanceId(delegateExecution.getProcessInstanceId());
+                infringementService.creatInfringementAction(infringement, delegateExecution.getProcessInstanceId(),
+                                delegateExecution.getVariable("infringementNotes").toString(),
+                                InfringementActionType.INFRINGEMENT_NOTIFICATION_SENT);
 
-        emailDispatcher.send(new Notification().setSubject("New Infringement Notification").setToFrom(fromEmail)
-                .setToEmail(driver.getEmail())
-                .setBody("A new infringement created." + infringement.getInfringementType()).setAction(taskUrl)
-                .setActionDescription("View Online"));
+                LOGGER.info("Sending SMS smsEnabled:.." + smsEnabled);
+                if ("true".equals(smsEnabled)) {
+                        LOGGER.info("Sending SMS smsEnabled inside:" + smsEnabled);
+                        String message = "A new infringement created." + infringement.getInfringementType()
+                                        + ". Click the following for more details, " + taskUrl + '.';
+                        smsSenderImpl.sendSms(new SmsMessage(driver.getCellNumber(), message));
+                }
 
-        if ("true".equals(smsEnabled)) {
-                String message = "A new infringement created." + infringement.getInfringementType() + ". Click the following for more details, " + taskUrl + '.';
-                smsSenderImpl.sendSms(new SmsMessage(driver.getCellNumber(), message));
+                emailDispatcher.send(new Notification().setSubject("New Infringement Notification").setToFrom(fromEmail)
+                                .setToEmail(driver.getEmail())
+                                .setBody("A new infringement created." + infringement.getInfringementType())
+                                .setAction(taskUrl).setActionDescription("View Online"));
+
         }
-
-    }
 }
